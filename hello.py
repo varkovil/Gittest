9a2ab4f8264e3ab77d20a7a4554faac744c4612e
Varun K <varun.kovoor@gmail.com>
2026-04-09 18:26:42 +0530

    adding skills

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ course='mlops'
 print(f"Hello! My name is {name}.")
 print(f"I am learning {course}.")
 print("This file is tracked by Git.")
+
+skills = ["Python", "Git", "GitHub", "AWS", "Docker"]
+print(f"My skills: {', '.join(skills)}")
